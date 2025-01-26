04d25328278cce9797814b5808c6280588fef29b
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-01-26 14:57:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ document.getElementById("next").onclick = function(){nextSlide();}
 let container = document.getElementById("slide-container");
 let voice = document.getElementById("voice");
 let slide = document.createElement("img");
-let imgOne = "/one.png";
-let imgTwo = ".two.png";
-let imgThree = "./three.png";
+let imgOne = "one.png";
+let imgTwo = "two.png";
+let imgThree = "three.png";
 let collection = [imgOne,imgTwo,imgThree];
-let audioCollection = ["recording.m4a", "voiceTwo.m4a", "voiceThree.m4a"]
+let audioCollection = ["voiceOne.mp3", "voiceTwo.mp3", "voiceThree.mp3"]
 let currentSlide = -1;
 
 
 function nextSlide(){
-container.innerHTML="";
+
 if(currentSlide >= collection.length -1){
     currentSlide = 0;
 }else{
@@ -25,7 +25,7 @@ container.appendChild(slide);
 voice.src =audioCollection[currentSlide];
 }
 function prevSlide(){
-    container.innerHTML="";
+   
     if(currentSlide < 1){
         currentSlide = collection.length-1;
     }else{
